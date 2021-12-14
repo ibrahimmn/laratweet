47196c7192e8a3ae22282017ab7ba10e7c743eb7
the bob <ibrahim.mneimneh3@gmail.com>
2021-12-14 16:41:46 +0300

    adding follow and unfollow methods to usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function follows($username)
+{
+    
+    $user = User::where('username', $username)->firstOrFail();
+
+    $id = Auth::id();
+    $me = User::find($id);
+    $me->following()->attach($user->id);
+    return redirect('/' . $username);
+}
+public function unfollows($username)
+{
+
+    $user = User::where('username', $username)->firstOrFail();
+
+    $id = Auth::id();
+    $me = User::find($id);
+    $me->following()->detach($user->id);
+    return redirect('/' . $username);
+}
 }
