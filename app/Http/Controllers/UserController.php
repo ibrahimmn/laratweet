c329811a462f12f458577a246bdf8d2972fa6968
the bob <ibrahim.mneimneh3@gmail.com>
2021-12-21 16:08:11 +0300

    still working on error(Cannot declare class App\User, because the name is already in use)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,28 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
-    public function follows($username)
-{
-    
-    $user = User::where('username', $username)->firstOrFail();
+    public function follows(Request $request)
+    {
+		$username =  $request->input('username');
 
-    $id = Auth::id();
-    $me = User::find($id);
-    $me->following()->attach($user->id);
-    return redirect('/' . $username);
-}
-public function unfollows($username)
-{
+    	try {
+	    	$user = User::where('username', $username)->firstOrFail();
+    	} catch (ModelNotFoundException $exp) {
+    		return $this->responseFail('User doesn\'t exists');
+    	}
 
-    $user = User::where('username', $username)->firstOrFail();
+    	// Find logged in User
+    	$id = Auth::id();
+    	$me = User::find($id);
 
-    $id = Auth::id();
-    $me = User::find($id);
-    $me->following()->detach($user->id);
-    return redirect('/' . $username);
-}
+    	$me->following()->attach($user->id);
+
+    	return $this->responseSuccess();
+    }
+
+    public function unfollows(Request $request)
+    {
+		$username =  $request->input('username');
+
+    	try {
+	    	$user = User::where('username', $username)->firstOrFail();
+    	} catch (ModelNotFoundException $exp) {
+    		return $this->responseFail('User doesn\'t exists');
+    	}
+
+    	// Find logged in User
+    	$id = Auth::id();
+    	$me = User::find($id);
+
+    	$me->following()->detach($user->id);
+
+    	return $this->responseSuccess();
+    }
+
+    private function responseSuccess($message = '')
+    {
+    	return $this->response(true, $message);
+    }
+
+    private function responseFail($message = '')
+    {
+    	return $this->response(false, $message);
+    }
+
+    private function response($status = false, $message = '')
+    {
+    	return response()->json([
+    		'status' => $status,
+    		'message' => $message,
+    		]);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,45 +1,34 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var string[]
+     * @var array
      */
     protected $fillable = [
-        'name',
-        'username',
-        'email',
-        'password',
+        'name', 'username', 'email', 'password',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+public function following()
+{
+    return $this->belongsToMany('App\User', 'followers', 'follower_user_id');
+}
 }
